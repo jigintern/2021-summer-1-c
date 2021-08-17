7914343d3563d083c19d6d509cff69a010ff1c94
uchuukaeru <hirata.motoki@gmail.com>
2021-08-17 13:14:30 +0900

    change server.js
    location.json => data/location.json
    infomation.json => data/infomation.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import {Server} from "https://js.sabae.cc/Server.js";
 import {jsonfs} from "https://js.sabae.cc/jsonfs.js";
 
-const locafn="location.json";
-const infofn="infomation.json";
+const locafn="data/location.json";
+const infofn="data/infomation.json";
 let loca=jsonfs.read(locafn)||[];
 let info=jsonfs.read(infofn)||[];
 
