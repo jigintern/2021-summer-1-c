69905617f1b2d23a42147539373562ca44e9abb7
S0R4Suke <taunamayopad@gmail.com>
2021-08-17 11:58:38 +0900

    add locating

--- src/main.js
+++ src/main.js
@@ -1,7 +1,56 @@
-var map;
-function initMap(){
+function initMap(){    
+    var map;
     map = new google.maps.Map(document.getElementById('map'),{
-        center: {lat:35.446806, lng:139.636163},
-        zoom: 8
+        center: {lat:35.943297, lng:136.186513},
+        zoom: 18,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        scaleControl: false,
+        streetViewControl:false,
+        styles: [{
+            featureType: 'all',
+            elementType: 'labels',
+            stylers: [
+                { visibility: 'off' }
+            ]
+        }]
     });
+}
+function getMyPlace() {
+    var output = document.getElementById("result");
+    if (!navigator.geolocation){//Geolocation apiがサポートされていない場合
+      output.innerHTML = "<p>Geolocationはあなたのブラウザーでサポートされておりません</p>";
+      return;
+    }
+    function success(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      output.innerHTML = '<p>緯度 ' + lat + '° <br>経度 ' + lng + '°</p>';
+      var latlng = new google.maps.LatLng(lat,lng) ;
+      var map = new google.maps.Map( document.getElementById( 'map' ) , {
+        center: latlng,
+        zoom: 18,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        scaleControl: false,
+        streetViewControl:false,
+        styles: [{
+            featureType: 'all',
+            elementType: 'labels',
+            stylers: [
+                { visibility: 'off' }
+            ]
+        }]  
+      });
+      new google.maps.Marker( {
+          map: map ,
+          position: latlng ,
+      });
+    };
+    function error() {
+      output.innerHTML = "座標位置を取得できません";
+    };
+    navigator.geolocation.getCurrentPosition(success, error);
 }
