1884e850c306d9d326c4f9f141f063a5d22c9827
S0R4Suke <taunamayopad@gmail.com>
2021-08-18 00:10:03 +0900

    AutoLocationPosition

--- src/main.js
+++ src/main.js
@@ -1,64 +1,61 @@
 function initMap(){
-    var map;
-    var lat = 35.94292;
-    var lng = 136.186513;
+    if (typeof(navigator.geolocation) != 'undefined') {
+        navigator.geolocation.watchPosition(success, error);
+    }
+}
+function success(position){
+    var targetlat = 35.94292;
+    var targetlng = 136.186513;
+    var targetlatlng = new google.maps.LatLng(targetlat,targetlng);
+
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
     var latlng = new google.maps.LatLng(lat,lng);
-    var infowindow;
-    var nowlat;
-    var nowlng;
-    var nowlatlng;
-    if( navigator.geolocation ){
-        navigator.geolocation.getCurrentPosition(
-            function( position ){
-                var data = position.coords ;
-                nowlat = data.latitude ;
-                nowlng = data.longitude ;
-                nowlatlng = new google.maps.LatLng( nowlat,nowlng ) ;
-                map = new google.maps.Map(document.getElementById('map'),{
-                    center: nowlatlng,
-                    zoom: 17,
-                    mapTypeControl: false,
-                    panControl: false,
-                    zoomControl: false,
-                    scaleControl: false,
-                    streetViewControl:false,
-                    styles: [{
-                        featureType: 'all',
-                        elementType: 'labels',
-                        stylers: [
-                            { visibility: 'off' }
-                        ]
-                    }]
-                });
-                var targetMarker = new google.maps.Marker( {
-                    map: map ,
-                    position: latlng ,
-                });
-                var myMarker = new google.maps.Marker({
-                    map: map,
-                    position: nowlatlng,
-                });
-                var myInfoWindow = new google.maps.InfoWindow({
-                    content:'現在地です'
-                });
-                var targetInfoWindow = new google.maps.InfoWindow({
-                    content:'めがね会館です'
-                });
 
-                targetInfoWindow.open(map,targetMarker);
-                google.maps.streetViewControl.addListener(targetInfoWindow,"closeclick",function(){
-                    google.maps.event.addListenerOnce(targetMarker,"click",function(event){
-                        myInfoWindow.open(map,targetMarker);
-                    });
-                });
+    var map = new google.maps.Map(document.getElementById('map'),{
+        center: latlng,
+        zoom: 17,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        scaleControl: false,
+        streetViewControl:false,
+        styles: [{
+            featureType: 'all',
+            elementType: 'labels',
+            stylers: [
+                { visibility: 'off' }
+            ]
+        }]
+    });
+    var TargetMarker = new google.maps.Marker({
+        map: map,
+        position: targetlatlng,
+    });
+    var marker = new google.maps.Marker({
+        map: map,
+        position: latlng,
+    });
+    if(lat>targetlat){
+        var maxlat = lat;
+        var minlat = targetlat;
+    } else {
+        var maxlat = targetlat;
+        var minlat = lat;
+    }
 
-                // myInfoWindow.open(map,myMarker);
-                // google.maps.streetViewControl.addListener(myInfoWindow,"closeclick",function(){
-                //     google.maps.event.addListenerOnce(myMarker,"click",function(event){
-                //         myInfoWindow.open(map,myMarker);
-                //     });
-                // });
-            } 
-        );
-    } 
+    if(lng>targetlng){
+        var maxlng = lng;
+        var minlng = targetlng;
+    }else{
+        var maxlng = targetlng;
+        var minlng = lng;
+    }
+    var sw = new google.maps.LatLng(maxlat,minlng);
+    var ne = new google.maps.LatLng(minlat,maxlng);
+    var bounds = new google.maps.LatLngBounds(sw, ne);
+    map.fitBounds(bounds,5);
+}
+function error(e){
+    alert("エラーが発生しました - " + e.message);
 }
