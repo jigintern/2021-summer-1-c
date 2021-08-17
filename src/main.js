7fd9b7f20d07c384b88effac4f81e97209d09588
S0R4Suke <taunamayopad@gmail.com>
2021-08-17 15:30:07 +0900

    目的地と現在地のマーカーを追加

--- src/main.js
+++ src/main.js
@@ -1,56 +1,64 @@
-function initMap(){    
+function initMap(){
     var map;
-    map = new google.maps.Map(document.getElementById('map'),{
-        center: {lat:35.943297, lng:136.186513},
-        zoom: 18,
-        mapTypeControl: false,
-        panControl: false,
-        zoomControl: false,
-        scaleControl: false,
-        streetViewControl:false,
-        styles: [{
-            featureType: 'all',
-            elementType: 'labels',
-            stylers: [
-                { visibility: 'off' }
-            ]
-        }]
-    });
-}
-function getMyPlace() {
-    var output = document.getElementById("result");
-    if (!navigator.geolocation){//Geolocation apiがサポートされていない場合
-      output.innerHTML = "<p>Geolocationはあなたのブラウザーでサポートされておりません</p>";
-      return;
-    }
-    function success(position) {
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-      output.innerHTML = '<p>緯度 ' + lat + '° <br>経度 ' + lng + '°</p>';
-      var latlng = new google.maps.LatLng(lat,lng) ;
-      var map = new google.maps.Map( document.getElementById( 'map' ) , {
-        center: latlng,
-        zoom: 18,
-        mapTypeControl: false,
-        panControl: false,
-        zoomControl: false,
-        scaleControl: false,
-        streetViewControl:false,
-        styles: [{
-            featureType: 'all',
-            elementType: 'labels',
-            stylers: [
-                { visibility: 'off' }
-            ]
-        }]  
-      });
-      new google.maps.Marker( {
-          map: map ,
-          position: latlng ,
-      });
-    };
-    function error() {
-      output.innerHTML = "座標位置を取得できません";
-    };
-    navigator.geolocation.getCurrentPosition(success, error);
+    var lat = 35.94292;
+    var lng = 136.186513;
+    var latlng = new google.maps.LatLng(lat,lng);
+    var infowindow;
+    var nowlat;
+    var nowlng;
+    var nowlatlng;
+    if( navigator.geolocation ){
+        navigator.geolocation.getCurrentPosition(
+            function( position ){
+                var data = position.coords ;
+                nowlat = data.latitude ;
+                nowlng = data.longitude ;
+                nowlatlng = new google.maps.LatLng( nowlat,nowlng ) ;
+                map = new google.maps.Map(document.getElementById('map'),{
+                    center: nowlatlng,
+                    zoom: 17,
+                    mapTypeControl: false,
+                    panControl: false,
+                    zoomControl: false,
+                    scaleControl: false,
+                    streetViewControl:false,
+                    styles: [{
+                        featureType: 'all',
+                        elementType: 'labels',
+                        stylers: [
+                            { visibility: 'off' }
+                        ]
+                    }]
+                });
+                var targetMarker = new google.maps.Marker( {
+                    map: map ,
+                    position: latlng ,
+                });
+                var myMarker = new google.maps.Marker({
+                    map: map,
+                    position: nowlatlng,
+                });
+                var myInfoWindow = new google.maps.InfoWindow({
+                    content:'現在地です'
+                });
+                var targetInfoWindow = new google.maps.InfoWindow({
+                    content:'めがね会館です'
+                });
+
+                targetInfoWindow.open(map,targetMarker);
+                google.maps.streetViewControl.addListener(targetInfoWindow,"closeclick",function(){
+                    google.maps.event.addListenerOnce(targetMarker,"click",function(event){
+                        myInfoWindow.open(map,targetMarker);
+                    });
+                });
+
+                // myInfoWindow.open(map,myMarker);
+                // google.maps.streetViewControl.addListener(myInfoWindow,"closeclick",function(){
+                //     google.maps.event.addListenerOnce(myMarker,"click",function(event){
+                //         myInfoWindow.open(map,myMarker);
+                //     });
+                // });
+            } 
+        );
+    } 
 }
