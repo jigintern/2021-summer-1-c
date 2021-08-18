ec8bca7a13d7334c4dc293eb9c4d1a345de631b7
uchuukaeru <hirata.motoki@gmail.com>
2021-08-18 11:37:08 +0900

    add api
    /api/add
    位置情報をuserdata.json保存するapiを追加

--- server.js
+++ server.js
@@ -17,25 +17,43 @@ let udata=jsonfs.read(udatafn)||[];
 class MyServer extends Server{
     api(path,reqID){
         
-        if(path=="/api/loca"){//指定されたIDのlocation.jsonを返す
+        if(path=="/api/loca"){
+            //指定されたIDのlocation.jsonを返す
+            //call:("/api/add",ID),return:{ID,name,lat,lng}
             for(const d of loca){
                 if(d.ID==reqID){
                     return d;
                 }
             }
             return "warning";
-        }else if(path=="/api/info"){//指定されたIDのinfomation.jsonを返す
+        }else if(path=="/api/info"){
+            //指定されたIDのinfomation.jsonを返す
+            //call:("/api/info",ID),return:{ID,info[]}
             for(const d of info){
                 if(d.ID==reqID){
                     return d;
                 }
             }
             return "warning";
-        } else if (path == "/api/icon") {//今持つすべてのicon.jsonの情報を返す
+        } else if (path == "/api/icon") {
+            //今持つすべてのicon.jsonの情報を返す
+            //call:("/api/icon"),return:[{iconID,addres}]
             return icon;
-        } else if (path == "api/add") {//位置情報を保存する
-
-        } else if (path == "/api/dist") {//
+        }else if (path == "/api/add") {//位置情報を保存する
+            //call:("/api/add",{travelID,data[{type,date,iconID,lat,lng}]}),return:ok
+            for(const d in udata){
+               if(udata[d].travelID==req.travelID){
+                    udata[d].data.push(req.data);
+                    jsonfs.write(udatafn,udata);
+                    return "push ok";
+               }
+           }
+           udata.push({travelID:req.travelID,data:[]});
+           udata[udata.length-1].data.push(req.data);
+           jsonfs.write(udatafn,udata);
+           return "make new record. push ok";
+        } else if (path == "/api/dist") {
+            //距離を求める
             let dist = 0;
             const origin = reqID.origin;
             const destination = reqID.destination;
