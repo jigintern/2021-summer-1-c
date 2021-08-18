44f5aa1c9773d83c28928a60fd912a23f86bf4c5
uchuukaeru <hirata.motoki@gmail.com>
2021-08-18 09:36:31 +0900

    Create home.jpg

--- server.js
+++ server.js
@@ -4,29 +4,34 @@ import {jsonfs} from "https://js.sabae.cc/jsonfs.js";
 const locafn="data/location.json";
 const infofn="data/infomation.json";
 const iconfn="data/icon.json";
+const udatafn="data/userdata.json";
+
 let loca=jsonfs.read(locafn)||[];
 let info=jsonfs.read(infofn)||[];
 let icon=jsonfs.read(iconfn)||[];
+let udata=jsonfs.read(udatafn)||[];
 
 class MyServer extends Server{
     api(path,reqID){
         
-        if(path=="/api/loca"){//
+        if(path=="/api/loca"){//指定されたIDのlocation.jsonを返す
             for(const d of loca){
                 if(d.ID==reqID){
                     return d;
                 }
             }
             return "warning";
-        }else if(path=="/api/info"){//
+        }else if(path=="/api/info"){//指定されたIDのinfomation.jsonを返す
             for(const d of info){
                 if(d.ID==reqID){
                     return d;
                 }
             }
             return "warning";
-        } else if (path == "/api/icon") {//
+        } else if (path == "/api/icon") {//今持つすべてのicon.jsonの情報を返す
             return icon;
+        } else if (path == "api/add") {//位置情報を保存する
+
         }
     }
 }
